59db4840e0d01f182c639a1ddd2270675ba4e4fb
Attila Horvath <sweeptic@gmail.com>
2025-03-13 10:57:22 +0100

    017 Loading Paths Dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -16,15 +16,21 @@ export default function ProductDetail(props) {
   );
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  //const data = undefined;
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  const jsonData = await fs.readFile(filePath);
-  //const data = undefined;
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((item) => item.id === productId);
 
@@ -37,6 +43,13 @@ export async function getStaticProps(context) {
 
 // 3x hivja a getstaticpropsot mert itt 3 path van
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+
+  const paths = ids.map((pid) => ({ params: { pid } }));
+
+  return { paths, fallback: 'blocking' };
+
   return {
     paths: [
       //    pre generate
