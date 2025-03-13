14aec1c0b0dde9227d3ca7cf84076db5c38500ec
Attila Horvath <sweeptic@gmail.com>
2025-03-13 12:38:18 +0100

    021 getServerSideProps and its Context

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -3,6 +3,12 @@ export default function UserProfilePage(props) {
 }
 
 export async function getServerSideProps(context) {
+  // full req object
+  const { params, req, res } = context;
+  //   console.log('params', params);
+  console.log('req', req.cookies);
+  //   console.log('res', res);
+
   return {
     props: {
       username: 'Max',
