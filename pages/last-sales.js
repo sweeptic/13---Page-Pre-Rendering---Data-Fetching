0436b32153d600b95baa4a22a1e8766b6983b034
Attila Horvath <sweeptic@gmail.com>
2025-03-13 14:32:42 +0100

    clean up

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -4,33 +4,11 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function LastSalesPage() {
-  //   const [phones, setPhones] = useState([]);
-  //   const [isLoading, setIsLoading] = useState(false);
-
   const { data, error } = useSWR('https://api.restful-api.dev/objects', fetcher);
-  //   const { data, error, isLoading } = useSWR('/api/user', fetcher);
 
   useEffect(() => {
     console.log('data', data);
   }, [data]);
-
-  //   setPhones(data);
-
-  //   useEffect(() => {
-  //     async function fetchApi() {
-  //       setIsLoading(true);
-  //       const res = await fetch('https://api.restful-api.dev/objects');
-  //       const data = await res.json();
-  //       setPhones(data);
-  //       setIsLoading(false);
-  //     }
-
-  //     fetchApi();
-  //   }, []);
-
-  //   useEffect(() => {
-  //     console.log('phones state have changed', phones);
-  //   }, [phones]);
 
   if (error) {
     return <p>Failed to load</p>;
